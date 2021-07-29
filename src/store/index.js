79b87751e80660f4183cf01e33d185b8bb1badaf
Yean Seyha <yeanseyha7@gmail.com>
2021-07-29 15:29:29 +0700

    Add todo

--- src/store/index.js
+++ src/store/index.js
@@ -18,13 +18,19 @@ export default createStore({
   actions: {
     async fetchTodos({commit}){
       const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-      
       commit('setTodos', response.data)
+    },
+
+    async addTodo({commit}, title){
+      const response = await axios.post('https://jsonplaceholder.typicode.com/todos',{title , completed:false})
+      commit('newTodo', response.data)
     }
   },
   mutations: {
     // pass data(dotos) to state
-    setTodos: (state, todos)=>(state.todos = todos)
+    setTodos: (state, todos)=>(state.todos = todos),
+
+    newTodo: (state, todo)=>state.todos.unshift(todo)
   },
 
   getters:{
