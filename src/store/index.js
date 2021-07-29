0befb04fddad56e63f7681b6a6475f507da10e1c
Yean Seyha <yeanseyha7@gmail.com>
2021-07-29 15:00:11 +0700

    [Vuex] Fetch to do from api

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,36 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    todos:[
+      {
+        id: 1,
+        title: "Todo one"
+      },
+      {
+        id: 2,
+        title: "Todo two"
+      }
+    ]
+  },
+  
+  actions: {
+    async fetchTodos({commit}){
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+      
+      commit('setTodos', response.data)
+    }
   },
   mutations: {
+    // pass data(dotos) to state
+    setTodos: (state, todos)=>(state.todos = todos)
   },
-  actions: {
+
+  getters:{
+    allTodos: (state) => state.todos
   },
+  
   modules: {
   }
 })
